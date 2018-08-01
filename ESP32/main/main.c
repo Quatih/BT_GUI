c6772d2c50dbbbd6220810b26f3364ec3d4286b6
Quatih <hvatig@gmail.com>
2018-08-01 16:43:23 +0000

    attempt to custom SDP service 128 bit UUID

--- ESP32/main/main.c
+++ ESP32/main/main.c
@@ -1,21 +1,188 @@
+/*
+ * Copyright (C) 2014 BlueKitchen GmbH
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holders nor the names of
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ * 4. Any redistribution, use, or modification is done solely for
+ *    personal benefit and not for any commercial purpose or for
+ *    monetary gain.
+ *
+ * THIS SOFTWARE IS PROVIDED BY BLUEKITCHEN GMBH AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL MATTHIAS
+ * RINGWALD OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * Please inquire about commercial licensing options at 
+ * contact@bluekitchen-gmbh.com
+ *
+ */
+
+#define __BTSTACK_FILE__ "spp_counter.c"
+
+// *****************************************************************************
+/* EXAMPLE_START(spp_counter): SPP Server - Heartbeat Counter over RFCOMM
+ *
+ * @text The Serial port profile (SPP) is widely used as it provides a serial
+ * port over Bluetooth. The SPP counter example demonstrates how to setup an SPP
+ * service, and provide a periodic timer over RFCOMM.   
+ *
+ * @text Note: To test, please run the spp_counter example, and then pair from 
+ * a remote device, and open the Virtual Serial Port.
+ */
+// *****************************************************************************
 
 #include <inttypes.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
- 
+#include <math.h>
 #include "btstack.h"
 
+static int RFCOMM_SERVER_CHANNEL;
 #define HEARTBEAT_PERIOD_MS 1000
+static uint8_t SERVICE_UUID [] = {0x0f, 0xd5, 0xca, 0x36, 0x4e, 0x7d, 0x4f, 0x99, 0x82, 0xec, 0x28, 0x68, 0x26, 0x2b, 0xd4, 0xe4};
+static void packet_handler (uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size);
 
 static uint16_t rfcomm_channel_id;
+static uint8_t  spp_service_buffer[150];
+static btstack_packet_callback_registration_t hci_event_callback_registration;
 
-static int RFCOMM_SERVER_CHANNEL;
 
+/* @section SPP Service Setup 
+ *s
+ * @text To provide an SPP service, the L2CAP, RFCOMM, and SDP protocol layers 
+ * are required. After setting up an RFCOMM service with channel nubmer
+ * RFCOMM_SERVER_CHANNEL, an SDP record is created and registered with the SDP server.
+ * Example code for SPP service setup is
+ * provided in Listing SPPSetup. The SDP record created by function
+ * spp_create_sdp_record consists of a basic SPP definition that uses the provided
+ * RFCOMM channel ID and service name. For more details, please have a look at it
+ * in \path{src/sdp_util.c}. 
+ * The SDP record is created on the fly in RAM and is deterministic.
+ * To preserve valuable RAM, the result could be stored as constant data inside the ROM.   
+ */
+
+void create_custom_sdp_record(uint8_t *service, uint32_t service_record_handle, int rfcomm_channel, const char *name){
+	
+	uint8_t* attribute;
+	de_create_sequence(service);
+    
+    // 0x0000 "Service Record Handle"
+	de_add_number(service, DE_UINT, DE_SIZE_16, BLUETOOTH_ATTRIBUTE_SERVICE_RECORD_HANDLE);
+	de_add_number(service, DE_UINT, DE_SIZE_32, service_record_handle);
+    
+	// 0x0001 "Service Class ID List"
+	de_add_number(service,  DE_UINT, DE_SIZE_16, BLUETOOTH_ATTRIBUTE_SERVICE_CLASS_ID_LIST);
+    de_add_uuid128(service, SERVICE_UUID );
+	
+	// 0x0004 "Protocol Descriptor List"
+	de_add_number(service,  DE_UINT, DE_SIZE_16, BLUETOOTH_ATTRIBUTE_PROTOCOL_DESCRIPTOR_LIST);
+	attribute = de_push_sequence(service);
+	{
+		uint8_t* l2cpProtocol = de_push_sequence(attribute);
+		{
+			de_add_number(l2cpProtocol,  DE_UUID, DE_SIZE_16, BLUETOOTH_PROTOCOL_L2CAP);
+		}
+		de_pop_sequence(attribute, l2cpProtocol);
+		
+		uint8_t* rfcomm = de_push_sequence(attribute);
+		{
+			de_add_number(rfcomm,  DE_UUID, DE_SIZE_16, BLUETOOTH_PROTOCOL_RFCOMM);  // rfcomm_service
+			de_add_number(rfcomm,  DE_UINT, DE_SIZE_8,  rfcomm_channel);  // rfcomm channel
+		}
+		de_pop_sequence(attribute, rfcomm);
+	}
+	de_pop_sequence(service, attribute);
+	
+	// 0x0005 "Public Browse Group"
+	de_add_number(service,  DE_UINT, DE_SIZE_16, BLUETOOTH_ATTRIBUTE_BROWSE_GROUP_LIST); // public browse group
+	attribute = de_push_sequence(service);
+	{
+		de_add_number(attribute,  DE_UUID, DE_SIZE_16, BLUETOOTH_ATTRIBUTE_PUBLIC_BROWSE_ROOT );
+	}
+	de_pop_sequence(service, attribute);
+	
+	// 0x0006
+	de_add_number(service,  DE_UINT, DE_SIZE_16, BLUETOOTH_ATTRIBUTE_LANGUAGE_BASE_ATTRIBUTE_ID_LIST);
+	attribute = de_push_sequence(service);
+	{
+		de_add_number(attribute, DE_UINT, DE_SIZE_16, 0x656e);
+		de_add_number(attribute, DE_UINT, DE_SIZE_16, 0x006a);
+		de_add_number(attribute, DE_UINT, DE_SIZE_16, 0x0100);
+	}
+	de_pop_sequence(service, attribute);
+	
+	// 0x0009 "Bluetooth Profile Descriptor List"
+	de_add_number(service,  DE_UINT, DE_SIZE_16, BLUETOOTH_ATTRIBUTE_BLUETOOTH_PROFILE_DESCRIPTOR_LIST);
+	attribute = de_push_sequence(service);
+	{
+		uint8_t *Profile = de_push_sequence(attribute);
+		{
+            //de_add_uuid128(Profile, SERVICE_UUID );
+			de_add_number(Profile,  DE_UUID, DE_SIZE_16, BLUETOOTH_SERVICE_CLASS_SERIAL_PORT);
+			de_add_number(Profile,  DE_UINT, DE_SIZE_16, BLUETOOTH_SERVICE_CLASS_LAN_ACCESS_USING_PPP);
+		}
+		de_pop_sequence(attribute, Profile);
+	}
+	de_pop_sequence(service, attribute);
+	
+	// 0x0100 "ServiceName"
+	de_add_number(service,  DE_UINT, DE_SIZE_16, 0x0100);
+	de_add_data(service,  DE_STRING, strlen(name), (uint8_t *) name);
+}
+
+/* LISTING_START(SPPSetup): SPP service setup */ 
+static void service_setup(void){
+
+    // register for HCI events
+    hci_event_callback_registration.callback = &packet_handler;
+    hci_add_event_handler(&hci_event_callback_registration);
+
+    l2cap_init();
+
+    rfcomm_init();
+    rfcomm_register_service(packet_handler, RFCOMM_SERVER_CHANNEL, 0xffff);  // reserved channel, mtu limited by l2cap
+
+    // init SDP, create record for SPP and register with SDP
+    sdp_init();
+    memset(spp_service_buffer, 0, sizeof(spp_service_buffer));
+    create_custom_sdp_record(spp_service_buffer, 0x10001, RFCOMM_SERVER_CHANNEL, "BT_Sense");
+
+    sdp_register_service(spp_service_buffer);
+    //sdp_client_query_rfcomm_register_callback(handle_query_rfcomm_event , NULL);
+
+    printf("SDP service record size: %u\n", de_get_len(spp_service_buffer));
+}
+/* LISTING_END */
+
+/* @section Periodic Timer Setup
+ * 
+ * @text The heartbeat handler increases the real counter every second, 
+ * and sends a text string with the counter value, as shown in Listing PeriodicCounter. 
+ */
+
+/* LISTING_START(PeriodicCounter): Periodic Counter */ 
 static btstack_timer_source_t heartbeat;
 static char lineBuffer[30];
-
 static void  heartbeat_handler(struct btstack_timer_source *ts){
     static int counter = 0;
 
@@ -36,33 +203,47 @@ static void one_shot_timer_setup(void){
     btstack_run_loop_set_timer(&heartbeat, HEARTBEAT_PERIOD_MS);
     btstack_run_loop_add_timer(&heartbeat);
 }
+/* LISTING_END */
 
-static void packet_handler (uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size);
- 
-static btstack_packet_callback_registration_t hci_event_callback_registration;
-/*
-static void packet_handler (uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size){
- 
-   uint16_t rfcomm_channel_id;
- 
-   if (packet_type == HCI_EVENT_PACKET
-       && hci_event_packet_get_type(packet) == RFCOMM_EVENT_INCOMING_CONNECTION){
- 
-       rfcomm_channel_id = rfcomm_event_incoming_connection_get_rfcomm_cid(packet);
-       rfcomm_accept_connection(rfcomm_channel_id);
- 
-     }else if (packet_type == RFCOMM_DATA_PACKET){
- 
-        printf("Received data: '");
- 
-        for (int i=0;i<size;i++){
-             putchar(packet[i]);
-         }
- 
-          printf("\n----------------\n");
-    }
-}
-*/
+
+/* @section Bluetooth Logic 
+ * @text The Bluetooth logic is implemented within the 
+ * packet handler, see Listing SppServerPacketHandler. In this example, 
+ * the following events are passed sequentially: 
+ * - BTSTACK_EVENT_STATE,
+ * - HCI_EVENT_PIN_CODE_REQUEST (Standard pairing) or 
+ * - HCI_EVENT_USER_CONFIRMATION_REQUEST (Secure Simple Pairing),
+ * - RFCOMM_EVENT_INCOMING_CONNECTION,
+ * - RFCOMM_EVENT_CHANNEL_OPENED, 
+* - RFCOMM_EVETN_CAN_SEND_NOW, and
+ * - RFCOMM_EVENT_CHANNEL_CLOSED
+ */
+
+/* @text Upon receiving HCI_EVENT_PIN_CODE_REQUEST event, we need to handle
+ * authentication. Here, we use a fixed PIN code "0000".
+ *
+ * When HCI_EVENT_USER_CONFIRMATION_REQUEST is received, the user will be 
+ * asked to accept the pairing request. If the IO capability is set to 
+ * SSP_IO_CAPABILITY_DISPLAY_YES_NO, the request will be automatically accepted.
+ *
+ * The RFCOMM_EVENT_INCOMING_CONNECTION event indicates an incoming connection.
+ * Here, the connection is accepted. More logic is need, if you want to handle connections
+ * from multiple clients. The incoming RFCOMM connection event contains the RFCOMM
+ * channel number used during the SPP setup phase and the newly assigned RFCOMM
+ * channel ID that is used by all BTstack commands and events.
+ *
+ * If RFCOMM_EVENT_CHANNEL_OPENED event returns status greater then 0,
+ * then the channel establishment has failed (rare case, e.g., client crashes).
+ * On successful connection, the RFCOMM channel ID and MTU for this
+ * channel are made available to the heartbeat counter. After opening the RFCOMM channel, 
+ * the communication between client and the application
+ * takes place. In this example, the timer handler increases the real counter every
+ * second. 
+ *
+ * RFCOMM_EVENT_CAN_SEND_NOW indicates that it's possible to send an RFCOMM packet
+ * on the rfcomm_cid that is include
+
+ */ 
 
 /* LISTING_START(SppServerPacketHandler): SPP Server - Heartbeat Counter over RFCOMM */
 static void packet_handler (uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size){
@@ -139,105 +320,23 @@ static void packet_handler (uint8_t packet_type, uint16_t channel, uint8_t *pack
 /* LISTING_RESUME */ 
 }
 /* LISTING_END */
- 
 
- void create_sdp_record(uint8_t *service, uint32_t service_record_handle, int rfcomm_channel, const char *name){
-	
-	uint8_t* attribute;
-	de_create_sequence(service);
-    
-    // 0x0000 "Service Record Handle"
-	de_add_number(service, DE_UINT, DE_SIZE_16, BLUETOOTH_ATTRIBUTE_SERVICE_RECORD_HANDLE);
-	de_add_number(service, DE_UINT, DE_SIZE_32, service_record_handle);
-    
-	// 0x0001 "Service Class ID List"
-	de_add_number(service,  DE_UINT, DE_SIZE_16, BLUETOOTH_ATTRIBUTE_SERVICE_CLASS_ID_LIST);
-	attribute = de_push_sequence(service);
-	{
-		de_add_number(attribute,  DE_UUID, DE_SIZE_16, BLUETOOTH_SERVICE_CLASS_SERIAL_PORT );
-	}
-	de_pop_sequence(service, attribute);
-	
-	// 0x0004 "Protocol Descriptor List"
-	de_add_number(service,  DE_UINT, DE_SIZE_16, BLUETOOTH_ATTRIBUTE_PROTOCOL_DESCRIPTOR_LIST);
-	attribute = de_push_sequence(service);
-	{
-		uint8_t* l2cpProtocol = de_push_sequence(attribute);
-		{
-			de_add_number(l2cpProtocol,  DE_UUID, DE_SIZE_16, BLUETOOTH_PROTOCOL_L2CAP);
-		}
-		de_pop_sequence(attribute, l2cpProtocol);
-		
-		uint8_t* rfcomm = de_push_sequence(attribute);
-		{
-			de_add_number(rfcomm,  DE_UUID, DE_SIZE_16, BLUETOOTH_PROTOCOL_RFCOMM);  // rfcomm_service
-			de_add_number(rfcomm,  DE_UINT, DE_SIZE_8,  rfcomm_channel);  // rfcomm channel
-		}
-		de_pop_sequence(attribute, rfcomm);
-	}
-	de_pop_sequence(service, attribute);
-	
-	// 0x0005 "Public Browse Group"
-	de_add_number(service,  DE_UINT, DE_SIZE_16, BLUETOOTH_ATTRIBUTE_BROWSE_GROUP_LIST); // public browse group
-	attribute = de_push_sequence(service);
-	{
-		de_add_number(attribute,  DE_UUID, DE_SIZE_16, BLUETOOTH_ATTRIBUTE_PUBLIC_BROWSE_ROOT );
-	}
-	de_pop_sequence(service, attribute);
-	
-	// 0x0006
-	de_add_number(service,  DE_UINT, DE_SIZE_16, BLUETOOTH_ATTRIBUTE_LANGUAGE_BASE_ATTRIBUTE_ID_LIST);
-	attribute = de_push_sequence(service);
-	{
-		de_add_number(attribute, DE_UINT, DE_SIZE_16, 0x656e);
-		de_add_number(attribute, DE_UINT, DE_SIZE_16, 0x006a);
-		de_add_number(attribute, DE_UINT, DE_SIZE_16, 0x0100);
-	}
-	de_pop_sequence(service, attribute);
-	
-	// 0x0009 "Bluetooth Profile Descriptor List"
-	de_add_number(service,  DE_UINT, DE_SIZE_16, BLUETOOTH_ATTRIBUTE_BLUETOOTH_PROFILE_DESCRIPTOR_LIST);
-	attribute = de_push_sequence(service);
-	{
-		uint8_t *sppProfile = de_push_sequence(attribute);
-		{
-			de_add_number(sppProfile,  DE_UUID, DE_SIZE_16, BLUETOOTH_SERVICE_CLASS_SERIAL_PORT);
-			de_add_number(sppProfile,  DE_UINT, DE_SIZE_16, BLUETOOTH_SERVICE_CLASS_LAN_ACCESS_USING_PPP);
-		}
-		de_pop_sequence(attribute, sppProfile);
-	}
-	de_pop_sequence(service, attribute);
-	
-	// 0x0100 "ServiceName"
-	de_add_number(service,  DE_UINT, DE_SIZE_16, 0x0100);
-	de_add_data(service,  DE_STRING, strlen(name), (uint8_t *) name);
-}
-
+int btstack_main(int argc, const char * argv[]);
 int btstack_main(int argc, const char * argv[]){
- 
-    hci_event_callback_registration.callback = &packet_handler;
-    hci_add_event_handler(&hci_event_callback_registration);
- 
-    l2cap_init();
- 
-    rfcomm_init();
+    (void)argc;
+    (void)argv;
     RFCOMM_SERVER_CHANNEL = rand() % 30 + 1; // select a channel from 1 to 30
     one_shot_timer_setup();
-    
-    rfcomm_register_service(packet_handler, RFCOMM_SERVER_CHANNEL, 0xffff); 
- 
-    sdp_init();
- 
+    service_setup();
+
     gap_discoverable_control(1);
-    // service
-    uint32_t handle = sdp_create_service_record_handle();
-    sdp_register_service()
-
-    //
     gap_ssp_set_io_capability(SSP_IO_CAPABILITY_DISPLAY_YES_NO);
-    gap_set_local_name("RFCOMM Test 00:00:00:00:00:00");
+    gap_set_local_name("BT amp 00:00:00:00:00:00");
 
+    // turn on!
     hci_power_control(HCI_POWER_ON);
- 
+    
     return 0;
 }
+/* EXAMPLE_END */
+
