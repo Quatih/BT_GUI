d5ea6272f0354a211c2310696f95d04413d610e2
Quatih <hvatig@gmail.com>
2018-08-19 18:15:07 +0000

    removed time insertion from queue

--- ESP32/main/include/static_queue.h
+++ ESP32/main/include/static_queue.h
@@ -7,36 +7,27 @@
 #include <time.h>
 #include <sys/time.h>
 
-//#define malloc(params) pvPortMalloc
-
-#define QueueType int
-#define QueueTime uint32_t
-#define MAX_SIZE 10
 typedef struct {
-    QueueType data;
-    QueueTime time;
-} dequeue_tuple;
+    int data;
+    uint32_t time;
+} val_tuple;
+
+#define QueueType val_tuple
+#define QueueTime uint32_t
+#define MAX_SIZE 1000
 
 typedef struct {
     QueueType queue[MAX_SIZE];
-    QueueTime timestamp[MAX_SIZE]; 
     size_t size;
 } Static_Queue;
 
-void queueClear(Static_Queue* q){
-    for(size_t i = MAX_SIZE; i != 0; i--){
-        q->queue[i] = 0;
-    }
-    q->size = 0;
-}
-
 Static_Queue *queueCreate() {
-    //printf("Allocating %u\n", sizeof(Static_Queue));
+    printf("Allocating %u\n", sizeof(Static_Queue));
 	Static_Queue *queue = (Static_Queue*) malloc(sizeof (Static_Queue));
 	if(queue == NULL)
 		return NULL;
-	
-	queueClear(queue);
+
+    queue->size = 0;
 	return queue;
 }
 
@@ -48,26 +39,15 @@ void queueOverWrite(Static_Queue* q, QueueType data){
     q->queue[q->size] = data;
 }
 
-QueueTime get_msecs() {
-
-    struct timeval tv;
-
-    gettimeofday(&tv, NULL);
-    QueueTime ret = (QueueTime) (tv.tv_sec * 1000LL + (tv.tv_usec / 1000LL));
-    return ret;
-}
-
-dequeue_tuple dequeue(Static_Queue* q){
-    dequeue_tuple ret = {NULL, NULL};
+QueueType dequeue(Static_Queue* q){
+    QueueType ret;
     if(queueIsEmpty(q)){
         return ret;
     }
-    
-    ret.data = q->queue[0];
-    ret.time = q->timestamp[0];
+    ret = q->queue[0];
+
     for (size_t i = 0; i < q->size ; i++){
         q->queue[i] = q->queue[i+1];
-        q->timestamp[i] = q->timestamp[i+1];
     }
     q->size--;
     //printf("deq: %d\n", ret);
@@ -78,14 +58,12 @@ dequeue_tuple dequeue(Static_Queue* q){
 bool enqueue(Static_Queue* q, QueueType data){
     if( q->size < MAX_SIZE){
         q->queue[q->size] = data;
-        q->timestamp[q->size] = get_msecs();
         q->size++;
     }
     else{
         //printf("overload\n");
         dequeue(q);
         q->queue[MAX_SIZE-1] = data;
-        q->timestamp[MAX_SIZE-1] = get_msecs();
         q->size = MAX_SIZE;
     }
     //printf("Value %d added at %d\n", data, q->size-1);
